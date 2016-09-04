e9229a9429e41fc50bec1bfc567db61d1563ed3c
Yuan Lunchuan <799652013@qq.com>
2016-09-04 23:06:10 +0800

    1, Add mysql config.

--- searchDoctor/settings.py
+++ searchDoctor/settings.py
@@ -77,11 +77,12 @@ WSGI_APPLICATION = 'searchDoctor.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'searchDoctor',
+        'USER': 'yuanlunchuan',
+        'PASSWORD': '5yuanlunchuan',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
