70be16edda66491670c46cb76a402953e6c848a8
Yuan Lunchuan <799652013@qq.com>
2016-09-04 22:46:00 +0800

    1, Add bootstrap css.

--- searchDoctor/settings.py
+++ searchDoctor/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'searchDoctor.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/uploads/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
