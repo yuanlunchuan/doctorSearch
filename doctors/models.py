9bad6016a1233ca0816260ca07c156f3aaf58576
Yuan Lunchuan <799652013@qq.com>
2016-09-05 23:35:40 +0800

    1, Add hospital model.

--- doctors/models.py
+++ doctors/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Hospital(models.Model):
+  name = models.CharField(max_length=20, verbose_name='医院')
+  address = models.CharField(max_length=30, verbose_name='地址')
+  map_link = models.URLField(verbose_name='地图链接')
+  profile = models.TextField(verbose_name='医院简介')
+  introduction = models.TextField(verbose_name='医院介绍')
+  image = models.ImageField(upload_to='hospital/%Y/%m', default='hospital/default.png', max_length=200, blank=True, null=True, verbose_name='医院图片')
+
+  class Meta:
+    verbose_name = '医院'
+    verbose_name_plural = verbose_name
+
+  def __str__(self):
+    return self.name
