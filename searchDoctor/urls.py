be9977a4087304f73cbeeefd14f42519c16b2b20
Yuan Lunchuan <799652013@qq.com>
2016-09-05 23:47:27 +0800

    1, Add hospital upload image.

--- searchDoctor/urls.py
+++ searchDoctor/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url, include
-
+from django.conf import settings
 from django.contrib import admin
 
 urlpatterns = [
+  url(r'^uploads/hospital/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT,}),
   url(r'^admin/', admin.site.urls),
   url(r'^', include('doctors.urls')),
 ]
