0f78edb1c3769d711d14e781b0eb333bf76a7c57
Yuan Lunchuan <799652013@qq.com>
2016-09-11 23:47:49 +0800

    1, Add doctor show page.

--- doctors/urls.py
+++ doctors/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from . import views
+from doctors import views, doctors
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^doctors/$', doctors.show, name='doctor_index')
 ]
