91835178938a82d94ceba12f8e8830f2acb47282
Yuan Lunchuan <799652013@qq.com>
2016-09-13 00:30:01 +0800

    1, Add sider bar.

--- static/js/searchDoctor/index.js
+++ static/js/searchDoctor/index.js
@@ -25,12 +25,17 @@ var searchDoctorObject = {
     var self = searchDoctorObject;
     $('.float-layer').addClass('hidden');
   },
+  onInputBoxClicked: function(event){
+    var self = searchDoctorObject;
+    event.stopPropagation();
+  },
   initialize: function(){
     var self = searchDoctorObject;
     $(window).scroll(self.onWindowScrolled);
     $('.icon-area').on('click', self.onIconAreaClicked);
     $('.float-layer').on('click', self.onFloatLayerClicked);
     $('.close-icon').on('click', self.onClosedIconClicked);
+    $('.input-box').on('click', self.onInputBoxClicked)
   }
 };
 
