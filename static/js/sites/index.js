b81d687d85a2c4a1789bc812e7954cad6f421d3d
Yuan Lunchuan <799652013@qq.com>
2016-09-17 18:56:48 +0800

    1, Refactor sites static file.

---
+++ static/js/sites/index.js
@@ -0,0 +1,48 @@
+var searchDoctorObject = {
+  onWindowScrolled: function(){
+    var self = searchDoctorObject;
+    var scrollToTopDistance = $(document).scrollTop();
+    if(scrollToTopDistance>0&&$('#navbar').is('.hidden')){
+      $('#navbar').removeClass('hidden');
+    }
+    if(scrollToTopDistance==0&&!$('#navbar').is('.hidden')){
+      $('#navbar').addClass('hidden');
+    }
+  },
+  onIconAreaClicked: function(event){
+    var self = searchDoctorObject;
+    if($('.float-layer').is('.hidden')){
+      $('.float-layer').removeClass('hidden');
+    }else{
+      $('.float-layer').addClass('hidden');
+    }
+  },
+  onFloatLayerClicked: function(event){
+    var self = searchDoctorObject;
+    $('.float-layer').addClass('hidden');
+  },
+  onClosedIconClicked: function(event){
+    var self = searchDoctorObject;
+    $('.float-layer').addClass('hidden');
+  },
+  onInputBoxClicked: function(event){
+    var self = searchDoctorObject;
+    event.stopPropagation();
+  },
+  initialize: function(){
+    var self = searchDoctorObject;
+    new WOW().init();
+    if($(document).scrollTop()>0){
+      $('#navbar').removeClass('hidden');
+    }
+    $(window).scroll(self.onWindowScrolled);
+    $('.icon-area').on('click', self.onIconAreaClicked);
+    $('.float-layer').on('click', self.onFloatLayerClicked);
+    $('.close-icon').on('click', self.onClosedIconClicked);
+    $('.input-box').on('click', self.onInputBoxClicked)
+  }
+};
+
+$(document).ready(function(){
+  searchDoctorObject.initialize();
+});
