b27f916641630ff3a639768b90fdd733bf41b73e
Yuan Lunchuan <799652013@qq.com>
2016-09-20 23:00:01 +0800

    1, Add log.

--- doctors/views.py
+++ doctors/views.py
@@ -1,7 +1,10 @@
+import logging
 from django.shortcuts import render
 
+logger = logging.getLogger('doctors.views')
+
 def index(request):
-  return render(request, 'doctors/index.html', locals())
+  return render(request, 'doctors/index1.html', locals())
 
 def show(request, doctor_id="0"):
-  return render(request, 'doctors/show.html', locals())
+  return render(request, 'doctors/show2.html', locals())
--- sites/views.py
+++ sites/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+import logging
+
+logger = logging.getLogger('doctors.views')
 
 def index(request):
   return render(request, 'sites/index.html', locals())
