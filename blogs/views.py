377869b916ea599656293cf74f42bce00754d1a4
Yuan Lunchuan <799652013@qq.com>
2016-09-26 23:37:25 +0800

    1, Add blog module.

--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
 
 def index(request):
-  print("----------in blogs index")
   return render(request, 'blogs/index.html', {})
--- searchDoctor/settings.py
+++ searchDoctor/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'doctors',
     'sites',
+    'blogs',
 ]
 
 MIDDLEWARE_CLASSES = [
