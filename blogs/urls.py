94343ac26387110f38f3469e5ff421e545dc6db8
Yuan Lunchuan <799652013@qq.com>
2016-09-29 23:31:24 +0800

    1, Add blog detail page.

--- blogs/urls.py
+++ blogs/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
   url(r'^$', views.index, name = 'blog_index'),
+  url(r'^(?P<blog_id>[0-9]+)/$', views.show, name='blog_show')
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
   return render(request, 'blogs/index.html', {})
+
+def show(request, blog_id="0"):
+  return render(request, 'blogs/show.html', {})
