96da8a2f0f7537b53e462f41894cdc51a1ee3e12
Yuan Lunchuan <799652013@qq.com>
2016-10-06 11:53:43 +0800

    1, Finished sites index page.

--- sites/views.py
+++ sites/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from diseases.models import *
 import logging
 
 logger = logging.getLogger('doctors.views')
 
 def index(request):
+  diseases_categories = DiseaseCategory.objects.all()
   return render(request, 'sites/index.html', locals())
